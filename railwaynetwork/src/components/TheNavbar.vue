cdb60574f84d172dcb9382ce7f0c4d9417ceaa45
Soltész Dávid <dolfismsh@gmail.com>
2022-10-14 10:45:18 +0200

    navbar fix

--- railwaynetwork/src/components/TheNavbar.vue
+++ railwaynetwork/src/components/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="fixed top-0 grid w-full grid-rows-1 p-5 bg-transparent xxl:grid-cols-10 xl:grid-cols-11 lg:grid-cols-8 md:grid-cols-8 sm:5 xs:grid-cols-4">
-        <h2 class="col-span-1 col-start-1 p-2 mr-3 font-medium transition duration-200 ease-in-out border-2 border-solid rounded-md xl: border-lightgray text-lightgray drop-shadow-md font-mainfont hover:shadow-xl shadow-lightgray hover:bg-lightgray hover:text-darkgray">
+        <h2 class="col-span-1 col-start-1 p-2 mr-3 font-medium text-center transition duration-200 ease-in-out border-2 border-solid rounded-md xl: border-lightgray text-lightgray drop-shadow-md font-mainfont hover:shadow-xl shadow-lightgray hover:bg-lightgray hover:text-darkgray">
             14:35
         </h2>
         <button 
