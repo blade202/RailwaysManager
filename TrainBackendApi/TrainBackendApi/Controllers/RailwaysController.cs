bc04c6a81663f7d6635bcc099d18492fa3901cf9
blade202 <blade28202@gmail.com>
2022-10-24 22:01:01 +0200

    backend api update

---
+++ TrainBackendApi/TrainBackendApi/Controllers/RailwaysController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using TrainBackendApi.Models;
+using TrainBackendApi.Services;
+
+namespace TrainBackendApi.Controllers
+{
+    public class RailwaysController:ControllerBase
+    {
+        private readonly RailwaysService railwaysServic1e;
+        private readonly UserManager userManager;
+        private IMemoryCache _chache;
+
+        public RailwaysController(RailwaysService railwaysServic1e, UserManager userManager, IMemoryCache chache)
+        {
+            this.railwaysServic1e = railwaysServic1e;
+            this.userManager = userManager;
+            _chache = chache;
+        }
+        [Route("ReadRalways")]
+        public IActionResult Read()
+        {
+            railwaysServic1e.readrailways();
+            return Ok();
+        }
+        [Route("GetRoutes")]
+        public IActionResult GetRoutes(int depatciyiid,int arivelcityid)
+        {
+            string Chachekey = $"{depatciyiid} {arivelcityid}";
+            if(!_chache.TryGetValue(Chachekey,out List<ReturnRalway> returnrailways))
+            {
+                returnrailways = railwaysServic1e.Generateroutes(depatciyiid, arivelcityid);
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                    SlidingExpiration = TimeSpan.FromMinutes(2),
+
+                };
+                _chache.Set(Chachekey, returnrailways, cacheEntryOptions);
+            }
+            return Ok(returnrailways);
+        }
+    }
+}
---
+++ TrainBackendApi/TrainBackendApi/Models/ReturnRalway.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainBackendApi.Models
+{
+    public class ReturnRalway
+    {
+    
+        
+        public int Km { get; set; }
+   
+        public int Price { get; set; }
+      
+        public List<Railway> Railways { get; set; }
+
+        public ReturnRalway(List<Railway> railways)
+        {
+            this.Railways = railways;
+            Km = railways.Sum(x => x.KM);
+            Price = Km * 15;
+        }
+    }
+}
